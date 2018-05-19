a3d61cd0fc2cdf305f4ed8c814f74d411f59fce6
DESKTOP-BM7L711\Kat9 <leonard.novakovic@gmail.com>
2018-05-19 14:54:03 +0200

    Napravljena logika aplikacije

---
+++ Kalkulator/Kalkulator/Controllers/KalkulatorController.cs
@@ -0,0 +1,40 @@
+﻿using Kalkulator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kalkulator.Controllers
+{
+    public class KalkulatorController : Controller
+    {
+        // GET: Kalkulator
+        public ActionResult Index()
+        {
+            //string model = "Dobar dan Koprivnica";
+            //string[] model2 = new string[] { "Dobar", "dan", "Koprivnica" };
+            return View();
+
+        }
+        public ActionResult Kalkulator(KalkulatorModel model)
+        {
+            switch (model.Operacija)
+            {
+                case Operacija.Zbrajanje:
+                    model.Rezultat = model.PrviOperand + model.DrugiOperand;
+                    break;
+                case Operacija.Oduzimanje:
+                    model.Rezultat = model.PrviOperand - model.DrugiOperand;
+                    break;
+                case Operacija.Mnozenje:
+                    model.Rezultat = model.PrviOperand * model.DrugiOperand;
+                    break;
+                case Operacija.Dijeljenje:
+                    model.Rezultat = model.PrviOperand / model.DrugiOperand;
+                    break;
+            }
+            return View("Index", model);
+        }
+    }
+}
