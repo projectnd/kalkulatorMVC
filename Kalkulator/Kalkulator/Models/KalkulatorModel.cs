59f88f58a1ffdd40f1199388179ecdbfc1956ae3
DESKTOP-BM7L711\Kat9 <leonard.novakovic@gmail.com>
2018-05-19 14:53:04 +0200

    Napravljena klasa modela kalkulatora

---
+++ Kalkulator/Kalkulator/Models/KalkulatorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kalkulator.Models
+{
+    public enum Operacija
+    {
+        Zbrajanje ='+',
+        Oduzimanje = '-',
+        Mnozenje ='*',
+        Dijeljenje = '/'
+    }
+    public class KalkulatorModel
+    {
+        public decimal PrviOperand { get; set; }
+        public decimal DrugiOperand { get; set; }
+        public Operacija Operacija { get; set; }
+        public decimal Rezultat { get; set; }
+
+    }
+}
